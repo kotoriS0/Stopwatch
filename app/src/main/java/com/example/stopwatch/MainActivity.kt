f4364ff3f9ce5a891f52b2c0c7b8b8d49d8737fb
kotoriShoji <kotorishoji@gmail.com>
2022-09-20 10:09:04 -0700

    saveInstanceState shows right numbers when reset but starts timer a few seconds earlier not the same each time

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.os.SystemClock.elapsedRealtime
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -16,12 +17,18 @@ class MainActivity : AppCompatActivity() {
     companion object {
         // all static constants go here
         val TAG = "MainActivity"
+        val DB = "debug"
+
+        val STATE_TIME = "time"
+        val STATE_BASE = "stopwatch.base"
+        val STATE_IS_RUNNING = "isRunning"
     }
 
     lateinit var stopwatch: Chronometer
     lateinit var start: Button
     lateinit var reset: Button
 
+    var isRunning = false
     var time = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,18 +39,40 @@ class MainActivity : AppCompatActivity() {
 
         wireWidgets()
 
+        // restore instance state if it exists
+        if(savedInstanceState != null) {
+            time = savedInstanceState.getLong(STATE_TIME)
+            isRunning = savedInstanceState.getBoolean(STATE_IS_RUNNING)
+            //stopwatch.base = SystemClock.elapsedRealtime() - time
+            //time = SystemClock.elapsedRealtime()
+            if(isRunning) {
+                stopwatch.base = SystemClock.elapsedRealtime() - time
+                stopwatch.start()
+                start.setBackgroundColor(Color.rgb(224, 70, 50))
+                start.text = "stop"
+            }
+            else {
+                //stopwatch.base = stopwatch.base + SystemClock.elapsedRealtime() + time
+                stopwatch.base = savedInstanceState.getLong(STATE_BASE) +SystemClock.elapsedRealtime() - time
+            }
+        }
+
         start.setOnClickListener {
             if(start.text.equals("stop")) {
                 time = SystemClock.elapsedRealtime()
                 stopwatch.stop()
                 start.setBackgroundColor(Color.rgb(110, 0, 248))
                 start.text = "start"
+                isRunning = false
+                Log.d(DB, "stop time: $time")
             }
             else {
                 stopwatch.setBase(stopwatch.base + SystemClock.elapsedRealtime() - time)
                 stopwatch.start()
                 start.setBackgroundColor(Color.rgb(224, 70, 50))
                 start.text = "stop"
+                isRunning = true
+                Log.d(DB, "start base:${stopwatch.base}")
             }
         }
 
@@ -51,8 +80,30 @@ class MainActivity : AppCompatActivity() {
             stopwatch.setBase(SystemClock.elapsedRealtime());
             time = SystemClock.elapsedRealtime()
             stopwatch.stop();
+            start.setBackgroundColor(Color.rgb(110, 0, 248))
             start.text = "start"
+            isRunning = false
         }
+    }
+
+    // use to preserve state though orientation changes
+    override fun onSaveInstanceState(outState: Bundle) {
+        // calculate the display time if currently running
+        if(isRunning) {
+            time = SystemClock.elapsedRealtime() - stopwatch.base
+        }
+        else {
+            Log.d(DB, "stopped time: $time")
+            //time = SystemClock.elapsedRealtime() - stopwatch.base
+            //time = stopwatch.base
+            Log.d(DB, "stopped save time: $time")
+        }
+        // save key-value pairs to the bundle before superclass call
+        outState.putLong(STATE_TIME, time)
+        outState.putLong(STATE_BASE, stopwatch.base)
+        outState.putBoolean(STATE_IS_RUNNING, isRunning)
+
+        super.onSaveInstanceState(outState)
     }
 
     private fun wireWidgets() {
